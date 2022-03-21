fb57944c067bc255e75de44e6be788915678f8cb
jdluis <jdluisdev@gmail.com>
2022-03-21 16:34:47 +0000

    class Admin and function submit working

--- js/app.js
+++ js/app.js
@@ -3,77 +3,96 @@ let adminLog;
 let promotorLog;
 
 
-/*Datos provicionales del administrador principal*/
-let adminName = "007";
-let adminPass = "2211";
-
-/*Permite identificar cuando un Admin se ha logeado, devolviendo el valor true a la variable adminLogin, para mas adelante mostrar algo con ella*/
-function signInAdmin() {
-  adminName = prompt("Introduzca su nombre");
-  adminPass = prompt("Introduzca su contraseña");
-  alert("Admin registrado sadisfactoriamente");
-  console.log("Se ha registrado correctamente el admin: " + adminName);
-}
 
 
-
+/*ARRAYS*/
+const admins =[];
+const promotores = [];
+const clientes = [];
 
 
 
 /*************
-///CLASES//////
-**************/
+ ///FUNCIONES////// 
+ **************/
+    /*Permite registrar un nuevo admin */
+   function signInAdmin() {
+     let newAdmin = new Admin(
+      prompt("Indique el nombre de usuario que deseé"),
+      prompt("Indique una constraseña"),
+      prompt("Indique el nombre de su evento"),
+      prompt("Indique el correo"),
+     );
+     admins.push(newAdmin);
+
+     alert("Admin registrado sadisfactoriamente"),
+     console.log(`Se ha registrado correctamente. Su Nombre para ingresar es: ${newAdmin.userName}, el nombre de su evento es: ${newAdmin.eventName}, y su pass: ${newAdmin.password}`)
+    }    
+
+/*************
+ ///CLASES////// 
+ **************/
 
 //Creo que existia el extend pero no lo entiendo del todo aun, ya que si lo extiendo a la clase promotor, no estoy seguro de si todos los metodos se me pasan directamente
 // O si en el caso de que fuera asi como podria impedir que se pasen algunos metodos.
+
 class Admin {
-    constructor(userName,password) {
-        this.userName = userName;
-        this.password = password;
-    }
+    constructor(userName,password,mail,eventName) {
+      this.userName = userName;
+      this.password = password;
+      this.mail = mail;
+      this.eventName =eventName;
+    }    
 
     Login() {
-        let userName = prompt("Introduzca su nombre");
-        let password = prompt("Introduzca su contraseña");
+      let userName = prompt("Introduzca su nombre");
+      let password = prompt("Introduzca su contraseña");
       
-        if (this.userName == userName && this.password  == password) {
-          console.log("Login realizado correctamente");
-          alert("Bienvenido " + userName);
-          return (promotorLog = true);
-        } else {
-          alert("No se ha podido logear, consulte el log");
-          console.log(
-            "Los datos introducidos no corresponden con ningun Administrador"
-          );
+      if (this.userName == userName && this.password  == password) {
+        console.log("Login realizado correctamente");
+        alert("Bienvenido " + userName);
+        return (promotorLog = true);
+      } else {
+        alert("No se ha podido logear, consulte el log");
+        console.log(
+          "Los datos introducidos no corresponden con ningun Administrador"
+          );  
           return (promotorLog = false);
-        }
-      }
+        }  
+      }  
       
       //Simplemente uniendolo a un boton o algo funcionaria para cambiar el estado de log
-       Logout() {
-        alert("Te has deslogeado correctamente");
+      Logout() {
+        alert("Te has deslogeado correctamente"); 
         console.log("Admin " + userName + " deslogeado");
         return (promotorLog = false);
-      }
-
+      }  
       
-      addPromotor(newPromotor) {  //Quiero que al crearse un cliente este se una al array clientes de ese promotor
-        newPromotor = new Client()
-    }
-}
-
-class Promotor {
-
-    constructor(userName,password) {
+      
+      addPromotor() {    
+      let newPromotor = new Promotor(
+        prompt("Indique el nombre de usuario que deseé"),
+        prompt("Indique una constraseña"),
+       );
+       promotores.push(newPromotor);
+  
+       alert("Promotor registrado sadisfactoriamente"),
+       console.log(`Se ha registrado correctamente. Su Nombre para ingresar es: ${newPromotor.userName}, y su pass: ${newPromotor.password}`)
+    } 
+  };
+    
+    class Promotor {
+      
+      constructor(userName,password) {
         this.userName = userName;
         this.password = password;
-    }
-
-    //Metodo del Promotor para logearse a su cuenta
-     Login() {
+      }    
+      
+      //Metodo del Promotor para logearse a su cuenta
+      Login() {
         let userName = prompt("Introduzca su nombre");
         let password = prompt("Introduzca su contraseña");
-      
+        
         if (this.userName == userName && this.password  == password) {
           console.log("Login realizado correctamente");
           alert("Bienvenido " + userName);
@@ -82,63 +101,56 @@ class Promotor {
           alert("No se ha podido logear, consulte el log");
           console.log(
             "Los datos introducidos no corresponden con ningun Administrador"
-          );
-          return (promotorLog = false);
-        }
-      }
-      
-      //Simplemente uniendolo a un boton o algo funcionaria para cambiar el estado de log
-       Logout() {
-        alert("Te has deslogeado correctamente");
+            );  
+            return (promotorLog = false);
+          }  
+        }  
+        
+        //Simplemente uniendolo a un boton o algo funcionaria para cambiar el estado de log
+        Logout() {
+        alert("Te has deslogeado correctamente"); 
         console.log("Admin " + userName + " deslogeado");
         return (promotorLog = false);
-      }
+      }  
 
-
+      
       addClient(newClient) {  //Quiero que al crearse un cliente este se una al array clientes de ese promotor
         newClient = new Client();
         clientes.push(newClient);
-    }
-
-    mostrarClientes() {
+      }    
+      
+      mostrarClientes() {
         for (const cliente of clientes) {
-            console.log(`El cliente ${cliente} se ha cargado correctamente`)
-        }        
-    }
-
-    eliminarCLiente(){
+          console.log(`El cliente ${cliente} se ha cargado correctamente`)
+        }            
+      }    
+      
+      eliminarCLiente(){
         //Realizar codigo para eliminar un cliente de un array.
-    }
-};
-
-
-
-class Client {
-    constructor(fullName,email, country, phoneNumber) {
+      }    
+    };    
+    
+    
+    
+    class Client {
+      constructor(fullName,email, country, phoneNumber, processStatus) {
         this.fullName = fullName;
         this.email = email;
         this.phoneNumber = phoneNumber;
         this.country = country;
-    }
-};
+        this.processStatus = processStatus;
+      }
+    };    
+    
+    
+    /*********************************
+     * DATOS TEMPORALES/DEMOS/PRUEBAS *
+     * ******************************/
+    
+    /*Datos provicionales del administrador principal*/
+    let adminName = "007";
+    let adminPass = "2211";
 
-
-/*DATOS TEMPORALES/DEMOS*/
-
-let promotor1 = new Promotor ("Dani", "123");
-promotor1.Login();
-
-let client1 = new Client ("Jose Antonio Jesus", "joseantonio@gmail.com", 608991469);
-let client2 = new Client ("Paco Antonio Jesus", "joseantonio@gmail.com", 608991469);
-let client3 = new Client ("Lauro Antonio Jesus", "joseantonio@gmail.com", 608991469);
-
-/*ARRAYS*/
-const promotores = [promotor1]
-const clientes = [client1, client2, client3];
-
-
-
-
-/*CONSOLE PARA TESTING*/ 
-console.log(clientes);
-
+    /*CONSOLE PARA TESTING*/ 
+    console.log("Testing App List PROM by jdluis");
+    
