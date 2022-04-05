32ea6bdaf12221e43f8508a2b344b33905b4e5cd
jdluis <jdluisdev@gmail.com>
2022-04-05 23:11:11 +0100

    Clear code, and split in folders..news imports

--- js/main.js
+++ js/main.js
@@ -1,18 +1,48 @@
-import Evento, { testEvent } from "./Class/Event.js";
+import Event, {events, testEvent, signInNewEvent, createNewEvent,eventFound,loginToEvent, logOut } from "./Class/Event.js";
 import Promotor, { testPromotor } from "./Class/Promotor.js";
 import Client, { testClient } from "./Class/Client.js";
-import {CambiarCG} from "./functions/autoSlider.js";
+import { CambiarBG } from "./functions/autoSlider.js";
+import { openForm } from "./functions/btnActions.js"
 
 /*CONSOLE PARA TESTING*/
 console.log("Testing App List PROM by jdluis");
 
 
 
+
 //INIT FUNCTIONS
-CambiarCG();
-
-
-/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', 'assets/particles.json', function() {
-    console.log('callback - particles.js config loaded');
+InitApp();
+function InitApp () {
+  allStorage(); //Cargar en el array el localStorage
+  CambiarBG(); //AutoSlider for BG images
+  openForm(btnOpenFormNewEvent, newEventSection, aboutSection); //ID: form,sectionToOpen, sectionToClose)
+  openForm(btnBackToAbout, aboutSection, newEventSection);
+  openForm(btnBackToAboutFromLogin, aboutSection, loginSection);
+  openForm(btnToLoginSection, loginSection, aboutSection);
+  signInNewEvent();
+  loginToEvent();
+  
+  /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+  particlesJS.load("particles-js", "assets/particles.json", function () {
+    console.log("callback - particles.js config loaded");
   });
+  allStorage();
+}
+
+  //Carga el contenido de todo el local Storagw y lo guarda en el array admins
+  function allStorage() {
+    let keys = Object.keys(localStorage), i = keys.length;
+    
+    while (i--) {
+      events.push(JSON.parse(localStorage.getItem(keys[i])));
+    }
+    return events;
+  }
+
+
+
+
+
+
+
+
